82084a30cc69c7f1e780157bfbc78deb0d296740
nur66 <nuriswanto66@gmail.com>
2022-01-16 22:52:48 +0800

    pengeluaran

--- Page/Pengeluaran.jsx
+++ Page/Pengeluaran.jsx
@@ -1,10 +1,69 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import axios from 'axios'
+import React, {useState} from 'react'
+import { ScrollView, StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import { useNavigate } from 'react-router-native'
 
 const Pengeluaran = () => {
+    const [user, setUser] = useState("")
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+    let navigate = useNavigate()
+
+    // Clear
+    const clear = ()=> {
+        setUser('')
+        setTitle('')
+        setContent('')
+    }
+
+    // Save
+    const save = ()=> {
+        const fdata = {
+            // yang kiri ke database, kanan dari form
+            userid: user,
+            title: title,
+            content: content
+        }
+        try {
+            axios.post('https://sanctumtyo.herokuapp.com/api/article', fdata)
+            .then(()=>{
+                navigate('/list')
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Pengeluaranan comming</Text>
+            <ScrollView style={{padding:20}}>
+                <TextInput value={user} onChangeText={(e)=>{
+                    setUser(e)
+                }} style={styles.input} />
+
+                <TextInput value={title} onChangeText={(e)=>{
+                    setTitle(e)
+                }} 
+                style={styles.input}
+                />
+
+                <TextInput 
+                value={content}
+                multiline={true}
+                numberOfLines={4}
+                onChangeText={(e)=>{
+                    setContent(e)
+                }}
+                style={styles.input}
+                />
+
+                <View style={styles.tombol}>
+                    <Button title='Save' onPress={save} />
+                </View>
+                
+                    <Button title='Reset' style={{backgroundColor: 'yellow'}} onPress={clear} />
+                
+            </ScrollView>
         </View>
     )
 }
@@ -16,5 +75,17 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: 'black',
+        height: 40,
+        width: 300,
+        paddingHorizontal: 10,
+        marginBottom: 14,
+        color: 'red'
+    },
+    tombol: {
+        margin: 10
     }
 })
